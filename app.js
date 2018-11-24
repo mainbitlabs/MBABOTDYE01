851e550b37d68ad095789eb7a16c1e82e650517a
esanchezlMBT <esanchezl@mainbit.com.mx>
2018-11-23 22:20:36 -0600

    Dialogos 1.0 -
    OK

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ A simple echo bot for the Microsoft Bot Framework.
 var restify = require('restify');
 var builder = require('botbuilder');
 var botbuilder_azure = require("botbuilder-azure");
+var azurest = require('azure-storage');
+var tableService = azurest.createTableService("botdyesa01", "+F+IpcFtKyi6jrCm05KMCPYfIuG2J+ezhnAgqTvtwVAYKb/rmJvOKp4KuJ+Q44ie0HhPMKaFk3sSjvweQ/31Kw==");
 
 // Setup Restify Server
 var server = restify.createServer();
@@ -36,22 +38,62 @@ var tableStorage = new botbuilder_azure.AzureBotStorage({ gzipData: false }, azu
 var bot = new builder.UniversalBot(connector);
 bot.set('storage', tableStorage);
 
+
+var Choice = {
+    Si: 'Sí',
+    No: 'No'
+ };
+// El díalogo principal inicia aquí
 bot.dialog('/', [
-    function (session) {
-        builder.Prompts.text(session, "Hello... from Github What's your name?");
+    
+    function (session, results, next) {
+        // Primer diálogo    
+        builder.Prompts.text(session, '¿Cuál es el número de serie que deseas revisar?')
     },
     function (session, results) {
-        session.userData.name = results.response;
-        builder.Prompts.number(session, "Hi " + results.response + ", How many years have you been coding?"); 
+        // Segundo diálogo
+        session.dialogData.ticket = results.response;
+        builder.Prompts.text(session, '¿Cuál es el nombre del asociado?')
     },
     function (session, results) {
-        session.userData.coding = results.response;
-        builder.Prompts.choice(session, "What language do you code Node using?", ["JavaScript", "CoffeeScript", "TypeScript"]);
+        session.dialogData.asociado = results.response;
+        // Tercer diálogo
+        tableService.retrieveEntity("botdyesatb01", session.dialogData.asociado, session.dialogData.ticket, function(error, result, response) {
+            // var unlock = result.Status._;
+            if(!error ) {
+    
+                session.send(`Esta es la información relacionada: \n **Número de Serie: ${session.dialogData.ticket} \n Asociado: ${result.PartitionKey._}  \n Proyecto: ${result.Proyecto._} \n Estatus: ${result.Status._}.**`);
+                builder.Prompts.choice(session, 'Hola ¿Esta información es correcta?', [Choice.Si, Choice.No], { listStyle: builder.ListStyle.button });
+            }
+            else{
+                session.endDialog("**Error:** Los datos son incorrectos, intentalo nuevamente.");
+            }
+        });
     },
     function (session, results) {
-        session.userData.language = results.response.entity;
-        session.send("Got it... " + session.userData.name + 
-                    " you've been programming for " + session.userData.coding + 
-                    " years and use " + session.userData.language + ".");
+        var selection = results.response.entity;
+        switch (selection) {
+            // El díalogo desbloqueo inicia si el usuario presiona Desbloquear cuenta
+            case Choice.Si:
+            // return session.beginDialog('viaticos');
+            
+            tableService.retrieveEntity("botdyesatb01", session.dialogData.asociado, session.dialogData.ticket, function(error, result, response) {
+                // var unlock = result.Status._;
+                if(!error ) {
+        
+                    session.send(`¿Deseas adjutar documentación o evidencia?`);
+                    session.endDialog(`Hemos terminado gracias`);
+                }
+                else{
+                    session.endDialog("**Error:**");
+                }
+            });
+            break;
+            // El díalogo existe inicia si el usuario presiona Resetear contraseña
+            case Choice.No:
+            session.endDialog('Por favor vuelve a introducir correctamente la información.');
+            break;
+        }
+        
     }
 ]);
