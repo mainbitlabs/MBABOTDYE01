2aaa19daecd19fa5cd547d61580300be0a4b56bc
Esanchezl <42590200+esanchezlMBT@users.noreply.github.com>
2019-02-19 08:40:21 -0600

    Reliease 5.0
    Cambios cuentas correo, Área y Nombre de Usuario

--- app.js
+++ app.js
@@ -348,9 +348,9 @@ var Docs = {
                                 user: `${config.email1}`,
                                 pass: `${config.pass}`,
                             }, from: `${config.email1}`,
-                            to: `${config.email1, config.email2, config.email3, config.email4}  `,
+                            to: `${config.email1}, ${config.email2}, ${config.email3}, ${config.email4}  `,
                             subject: `${session.dialogData.proyecto} Incidente de ${session.dialogData.X}: ${session.dialogData.serie} / ${result.Servicio._}`,
-                            html: `<p>El servicio se pospuso por el siguiente motivo:</p> <br> <b>${session.dialogData.X}</b> <br> <b><blockquote>${session.dialogData.comentarios}</blockquote></b> <br> <b>Proyecto: ${session.dialogData.proyecto}</b>  <br> <b>Serie: ${session.dialogData.serie}</b> <br> <b>Servicio: ${result.Servicio._}</b> <br> <b>Localidad: ${result.Localidad._}</b> <br> <b>Inmueble: ${result.Inmueble._}</b>`
+                            html: `<p>El servicio se pospuso por el siguiente motivo:</p> <br> <b>${session.dialogData.X}</b> <br> <b><blockquote>${session.dialogData.comentarios}</blockquote></b> <br> <b>Proyecto: ${session.dialogData.proyecto}</b>  <br> <b>Serie: ${session.dialogData.serie}</b> <br> <b>Servicio: ${result.Servicio._}</b> <br> <b>Localidad: ${result.Localidad._}</b> <br> <b>Inmueble: ${result.Inmueble._}</b> <br> <b>Nombre de Usuario: ${result.NombreUsuario._}</b> <br> <b>Area: ${result.Area._}</b>`
                            });
                     }
                     else{
